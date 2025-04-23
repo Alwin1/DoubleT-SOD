e9a0c74fba901b8e0aee8b7ee099f51c19dcf55a
Alwin Tomy <alwintomy@Alwins-MacBook-Air.local>
2025-04-23 07:40:44 -0700

    added about pages

--- doublet-sod/src/App.jsx
+++ doublet-sod/src/App.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './home/Home';
-// import About from './about/About';
+import About from './about/About';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   </StrictMode>
---
+++ doublet-sod/src/about/About.jsx
@@ -0,0 +1,19 @@
+import './About.css'
+import Footer from '../overlays/Footer'
+import Header from '../overlays/Header'
+import DragImage from '../home/DragImage'
+
+function About() {
+
+  return (
+    <>
+      <div className='aboutContainer'>
+        <Header />
+        <DragImage />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default About
